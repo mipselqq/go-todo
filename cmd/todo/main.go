f0733562dcd3ddfdea3d8ea3c1b4ed23630d7a4a
Ilya Semyonov <mipselqq@gmail.com>
2025-12-23 19:25:15 +0300

    feat(main): implement graceful shutdown

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"context"
 	logging "go-todo/internal"
 	"go-todo/internal/handlers"
 	"go-todo/internal/services"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
 func main() {
 	logger_base := logging.NewLoggerBase()
+	logger := logger_base.With("component", "main")
+
 	mux := http.NewServeMux()
 
 	healthService := services.NewHealthCheckService(logger_base)
@@ -31,8 +36,20 @@ func main() {
 			os.Exit(1)
 		}
 	}()
-
 	logger_base.Info("Server started at", "addr", addr)
 
-	select {}
+	quitChan := make(chan os.Signal, 1)
+	signal.Notify(quitChan, os.Interrupt, syscall.SIGTERM)
+
+	<-quitChan
+	logger.Info("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		logger.Error("Server forced to shutdown", "err", err)
+	} else {
+		logger.Info("Server exited gracefully")
+	}
 }
