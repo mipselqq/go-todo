02b3f835463c99a79b76d223763900b8ff10f912
Ilya Semyonov <mipselqq@gmail.com>
2025-12-23 18:04:41 +0300

    feat(main): add basic logging

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"go-todo/internal/handlers"
+	"log/slog"
+	"net/http"
+	"os"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/health", handlers.HealthCheck)
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	addr := ":8080"
+	server := &http.Server{
+		Addr:         addr,
+		Handler:      mux,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			logger.Error("Failed to listen and serve", "err", err)
+			os.Exit(1)
+		}
+	}()
+
+	logger.Info("Server started at", "addr", addr)
+
+	select {}
 }
