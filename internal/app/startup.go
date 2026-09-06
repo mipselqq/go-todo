0a66eb0befda210cd405ad2473597d95fddf09c8
Ilya Semyonov <62264768+mipselqq@users.noreply.github.com>
2026-09-07 01:24:30 +0300

    refactor: shared migrations runner (#248)
    * WIP: refactor: remove migrations from app and remove depends_on for notify
    
    * refactor: add migrate binary and run it before services on deploy
    
    * refactor: add migrate-up-go which runs migrator for CI purposes
    
    * refactor: bring back migrations to CI
    
    * refactor: bring back migrations to CI
    
    * doc: add manual migration step to quickstart
    
    * fix: use godotenv.Load instead of xargs to parse migrator env
    
    * refactor: remove misleading logger.WithModule() calls

--- internal/app/startup.go
+++ internal/app/startup.go
@@ -12,14 +12,10 @@ import (
 	"goroutine/internal/logging"
 
 	"github.com/jackc/pgx/v5/pgxpool"
-	"github.com/jackc/pgx/v5/stdlib"
-	"github.com/pressly/goose/v3"
 	"github.com/redis/go-redis/v9"
 )
 
 func SetupPostgresFromEnv(logger *slog.Logger) (*pgxpool.Pool, error) {
-	logger = logging.WithModule(logger, "app.startup")
-
 	envConfig := config.NewPGFromEnv(logger)
 
 	logger.Info("Database config", slog.Any("config", envConfig))
@@ -56,22 +52,7 @@ func SetupPostgresFromEnv(logger *slog.Logger) (*pgxpool.Pool, error) {
 	return pool, nil
 }
 
-func MigratePostgres(pool *pgxpool.Pool, logger *slog.Logger, migrationsDir string) error {
-	logger = logging.WithModule(logger, "app.startup")
-
-	goose.SetLogger(&logging.GooseLogger{Base: logger})
-
-	err := goose.SetDialect("postgres")
-	if err != nil {
-		return err
-	}
-
-	return goose.Up(stdlib.OpenDBFromPool(pool), migrationsDir)
-}
-
 func SetupRedisFromEnv(logger *slog.Logger) (*redis.Client, error) {
-	logger = logging.WithModule(logger, "app.startup")
-
 	cfg := config.NewRedisFromEnv(logger)
 
 	logger.Info("Redis config", slog.Any("config", cfg))
